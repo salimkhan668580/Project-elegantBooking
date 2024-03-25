1df355cb9cec1aaaa2736c4225e02a56ae889842
Shivamganesh <118615006+Shivamganesh@users.noreply.github.com>
2024-03-25 02:28:20 -0600

    added login and home page

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/shivam', function(req, res) {
+  res.render("index");
 });
+router.post("/register",(req,res)=>{
+  
+})
+
 
 module.exports = router;
